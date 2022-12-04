2f1cae1c29d7da4108d3d658d4d0e4a219f8dd49
Maximus Johnston <50624061+MaximusJ08@users.noreply.github.com>
2022-12-04 13:51:52 +0000

    Advent of code part 4

---
+++ CleanUp2.java
@@ -0,0 +1,39 @@
+
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.util.Scanner;
+public class CleanUp2 {
+
+
+		public static void main(String[] args) {
+		    try {
+		        File myObj = new File("Cleanup.txt");
+		        Scanner myReader = new Scanner(myObj);
+		        int Overlap = 0;
+		        while (myReader.hasNextLine()) {
+			          String data = myReader.nextLine();
+			          String[] arrSplit = data.split(",");
+			          String[] oneSplit = arrSplit[0].split("-");
+			          String[] twoSplit = arrSplit[1].split("-");
+			          int oneOne = Integer.parseInt(oneSplit[0]);
+			          int oneTwo = Integer.parseInt(oneSplit[1]);
+			          int twoOne = Integer.parseInt(twoSplit[0]);
+			          int twoTwo = Integer.parseInt(twoSplit[1]);
+			          
+			          if((oneOne<=twoOne & oneTwo>=twoOne) || (oneOne>=twoOne & oneOne<=twoTwo) ) {
+			        	  Overlap++;
+			          }
+			          
+		        
+		        }
+		        System.out.print(Overlap);
+		        myReader.close();
+		        
+		      } catch (FileNotFoundException e) {
+		        System.out.println("An error occurred.");
+		        e.printStackTrace();
+		      }
+		}
+		
+	}
+
